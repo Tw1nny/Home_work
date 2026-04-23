5b0945b80dce3a0c4a8d25e85be48df0ecb3ac21
Сергей Беляев <tw1nny@yandex.ru>
2026-04-23 16:37:02 +0300

    Add tests and fix formatting

---
+++ tests/test_masks.py
@@ -0,0 +1,11 @@
+import pytest
+
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def test_get_mask_card_number():
+    assert get_mask_card_number("7000792289606361") == "7000 79** **** 6361"
+
+
+def test_get_mask_account():
+    assert get_mask_account("73654108430135874305") == "**4305"
